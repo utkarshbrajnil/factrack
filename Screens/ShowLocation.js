2bb058e9d241c5bb3e6487e9c4f47e82a7f463e3
Utkarsh Brajnil <brajnilutkarsh@gmail.com>
2020-12-15 00:03:15 +0530

    map search

--- Screens/ShowLocation.js
+++ Screens/ShowLocation.js
@@ -3,6 +3,7 @@ import { StyleSheet, Text, View, StatusBar } from 'react-native';
 import { SearchBar } from 'react-native-elements';
 import MapView from 'react-native-maps';
 import * as firebase from 'firebase';
+import { Marker } from 'react-native-maps'
 import firebaseConfig from '../firebaseConfig'
 
 import { LogBox } from 'react-native';
@@ -20,44 +21,60 @@ const height = StatusBar.currentHeight;
 
 
 const ShowLocation = () => {
+    const LAD = 0.1;
+    const LOD = 0.5;
     const [search, updateSearch] = useState('');
-    const [lat, setLat] = useState(23.4685);
-    const [long, setLong] = useState(74.09765);
-
-    useEffect(() => {
-        //firebase.initializeApp(firebaseConfig);
-
-        async function fetchData() {
+    const [lat, setLat] = useState();
+    const [long, setLong] = useState();
+    // var [region, setRegion] = useState({
+    //     latitude: lat,
+    //     longitude: long,
+    //     latitudeDelta: LAD,
+    //     longitudeDelta: LOD
+    //   });
+    async function fetchData(){
           
-          const response = await firebase.database().ref("faculties");
-            // response.on("value", snap => {
-            //     console.log(snap);
-            // })
-        }
-        fetchData();
-      }, []);
+        let lati = await firebase.database().ref("faculties").child(search).child("latitude");
+        let longi = await firebase.database().ref("faculties").child(search).child("longitude");
+           lati.on("value", lat => {
+           setLat(lat.val());
+           });
+           longi.on("value", long => {
+            setLong(long.val());
+        });
+
+      }
+
+    
 
     return(
         <>
             <SearchBar
+                returnKeyType='search'
                 placeholder="Search for a teacher..."
                 onChangeText={term => updateSearch(term)}
+                onSubmitEditing={() => fetchData()}
                 value={search}
                 containerStyle={styles.searchbar}
                 inputContainerStyle={styles.searchInput}
             />
             
-            <MapView 
-                showsUserLocation
-                style={styles.container} 
-                initialRegion= {{
-                    latitude: 19.6372,
-                    longitude: 72.998,
-                    latitudeDelta: 1,
-                    longitudeDelta: 1
-                }}
-                mapType="standard"
-            />
+            <MapView
+                style={styles.container}
+                region={{
+                    latitude: lat,
+                    longitude: long,
+                    latitudeDelta: LAD,
+                    longitudeDelta: LOD
+                  }}
+                
+            >
+                    <Marker 
+                        coordinate={{ latitude: lat, longitude: long }} 
+                        title={search}
+                        description="Currently at this location"
+                    />
+            </MapView>
         </>
         
     )
