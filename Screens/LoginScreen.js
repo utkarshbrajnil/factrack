0adc71e748b55d0051c30232cc042be605ca66b4
Aryan Thakre <aryan.thakre2018@vitstudent.ac.in>
2020-12-25 01:56:31 +0530

    new login UI

--- Screens/LoginScreen.js
+++ Screens/LoginScreen.js
@@ -1,17 +1,88 @@
 import React, { useState } from "react";
-import { View, StyleSheet, Text, ImageBackground, Image } from "react-native";
-import { Icon } from "react-native-elements";
-import { Container, Form, Input, Item, Button } from "native-base";
+import {
+  View,
+  StyleSheet,
+  Text,
+  StatusBar,
+  Dimensions,
+  TouchableOpacity,
+  TextInput,
+  Platform,
+  Image,
+} from "react-native";
+import * as Animatable from "react-native-animatable";
+import { LinearGradient } from "expo-linear-gradient";
+import FontAwesome from "react-native-vector-icons/FontAwesome";
+import Feather from "react-native-vector-icons/Feather";
 import firebase from "firebase";
 
-const image = {
-  uri:
-    "https://i.pinimg.com/originals/b9/16/73/b91673c3e31d8b688d4b5e28769eba67.png",
-};
+const { height } = Dimensions.get("screen");
+const height_logo = height * 0.28;
 
 export default function LoginScreen({ navigation }) {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const [data, setData] = React.useState({
+    username: "",
+    password: "",
+    check_textInputChange: false,
+    secureTextEntry: true,
+    isValidUser: true,
+    isValidPassword: true,
+  });
+
+  const textInputChange = (val) => {
+    if (val.trim().length >= 4) {
+      setData({
+        ...data,
+        username: val,
+        check_textInputChange: true,
+        isValidUser: true,
+      });
+    } else {
+      setData({
+        ...data,
+        username: val,
+        check_textInputChange: false,
+        isValidUser: false,
+      });
+    }
+  };
+
+  const handlePasswordChange = (val) => {
+    if (val.trim().length >= 8) {
+      setData({
+        ...data,
+        password: val,
+        isValidPassword: true,
+      });
+    } else {
+      setData({
+        ...data,
+        password: val,
+        isValidPassword: false,
+      });
+    }
+  };
+
+  const updateSecureTextEntry = () => {
+    setData({
+      ...data,
+      secureTextEntry: !data.secureTextEntry,
+    });
+  };
+
+  const handleValidUser = (val) => {
+    if (val.trim().length >= 4) {
+      setData({
+        ...data,
+        isValidUser: true,
+      });
+    } else {
+      setData({
+        ...data,
+        isValidUser: false,
+      });
+    }
+  };
 
   const loginUser = (navigation) => {
     firebase
@@ -29,199 +100,230 @@ export default function LoginScreen({ navigation }) {
   };
 
   return (
-    <>
-        <View style={styles.header}>
-          <Text style={styles.text_header}>TrackIT.</Text>
+    <View style={styles.container}>
+      <StatusBar backgroundColor="#7c73e6" barStyle="light-content" />
+      <View style={styles.header}>
+        <Animatable.Image
+          resizeMode="stretch"
+          style={styles.logo}
+          source={require("../assets/welcome.png")}
+          animation="bounceIn"
+          duration={1500}
+        />
+      </View>
+      <Animatable.View
+        animation="fadeInUpBig"
+        style={[
+          styles.footer,
+          {
+            backgroundColor: "#fafafa",
+          },
+        ]}
+      >
+        <Text
+          style={[
+            styles.text_footer,
+            {
+              color: "black",
+            },
+          ]}
+        >
+          Email
+        </Text>
+        <View style={styles.action}>
+          <FontAwesome name="user-o" color="black" size={20} />
+          <TextInput
+            placeholder="Your Email"
+            placeholderTextColor="#666666"
+            style={[
+              styles.textInput,
+              {
+                color: "black",
+              },
+            ]}
+            autoCapitalize="none"
+            onChangeText={(val) => textInputChange(val)}
+            onEndEditing={(e) => handleValidUser(e.nativeEvent.text)}
+          />
+          {data.check_textInputChange ? (
+            <Animatable.View animation="bounceIn">
+              <Feather name="check-circle" color="green" size={20} />
+            </Animatable.View>
+          ) : null}
         </View>
+        {data.isValidUser ? null : (
+          <Animatable.View animation="fadeInLeft" duration={500}>
+            <Text style={styles.errorMsg}>
+              Username must be 4 characters long.
+            </Text>
+          </Animatable.View>
+        )}
 
-         <View style={styles.footer}>
-          <Form style={{ marginVertical: 20, paddingHorizontal: 10 }}>
-            <Item
-              rounded
-              style={{ backgroundColor: "rgba(184, 196, 212,0.4)" }}
-            >
-              <Icon
-                name="email"
-                style={{ marginLeft: 10, opacity: 0.6 }}
-                color="white"
-              />
-              <Input
-                autoCorrect={false}
-                autoCapitalize="none"
-                placeholderTextColor="white"
-                placeholder="Enter email"
-                style={{ color: "white" }}
-                onChangeText={(email) => setEmail(email)}
-              />
-            </Item>
+        <Text
+          style={[
+            styles.text_footer,
+            {
+              color: "black",
+              marginTop: 35,
+            },
+          ]}
+        >
+          Password
+        </Text>
+        <View style={styles.action}>
+          <Feather name="lock" color="black" size={20} />
+          <TextInput
+            placeholder="Your Password"
+            placeholderTextColor="#666666"
+            secureTextEntry={data.secureTextEntry ? true : false}
+            style={[
+              styles.textInput,
+              {
+                color: "black",
+              },
+            ]}
+            autoCapitalize="none"
+            onChangeText={(val) => handlePasswordChange(val)}
+          />
+          <TouchableOpacity onPress={updateSecureTextEntry}>
+            {data.secureTextEntry ? (
+              <Feather name="eye-off" color="grey" size={20} />
+            ) : (
+              <Feather name="eye" color="grey" size={20} />
+            )}
+          </TouchableOpacity>
+        </View>
+        {data.isValidPassword ? null : (
+          <Animatable.View animation="fadeInLeft" duration={500}>
+            <Text style={styles.errorMsg}>
+              Password must be 8 characters long.
+            </Text>
+          </Animatable.View>
+        )}
 
-            <Item
-              rounded
-              style={{
-                marginTop: 10,
-                backgroundColor: "rgba(184, 196, 212,0.4)",
-              }}
-            >
-              <Icon
-                name="lock"
-                style={{ marginLeft: 10, opacity: 0.6 }}
-                color="white"
-              />
-              <Input
-                secureTextEntry
-                placeholderTextColor="white"
-                placeholder="Enter password"
-                style={{ color: "white" }}
-                autoCorrect={false}
-                autoCapitalize="none"
-                onChangeText={(pass) => setPassword(pass)}
-              />
-            </Item>
-
-            <Button
-              full
-              success
-              style={{
-                marginTop: 40,
-                borderRadius: 20,
-                backgroundColor: "#0F2C52",
-              }}
-              onPress={() => loginUser(navigation)}
+        <TouchableOpacity>
+          <Text style={{ color: "#7c73e6", marginTop: 15 }}>
+            Forgot password?
+          </Text>
+        </TouchableOpacity>
+        <View style={styles.button}>
+          <TouchableOpacity style={styles.signIn} onPress={() => loginUser()}>
+            <LinearGradient
+              colors={["#B0ABF0", "#7c73e6"]}
+              style={styles.signIn}
             >
               <Text
-                style={{
-                  color: "white",
-                  fontSize: 22,
-                  fontWeight: "bold",
-                  color: "#cccccc",
-                }}
+                style={[
+                  styles.textSign,
+                  {
+                    color: "#fff",
+                  },
+                ]}
               >
-                Log In
+                Sign In
               </Text>
-            </Button>
+            </LinearGradient>
+          </TouchableOpacity>
 
-            <Button
-              full
-              transparent
-              style={{ marginTop: 40, borderRadius: 20, marginTop: 5 }}
-              onPress={() => {
-                navigation.navigate("Signup");
-              }}
+          <TouchableOpacity
+            onPress={() => navigation.navigate("Signup")}
+            style={[
+              styles.signIn,
+              {
+                borderColor: "#7c73e6",
+                borderWidth: 1,
+                marginTop: 15,
+              },
+            ]}
+          >
+            <Text
+              style={[
+                styles.textSign,
+                {
+                  color: "#7c73e6",
+                },
+              ]}
             >
-              <Text style={{ color: "white", fontSize: 22, color: "#0F2C52" }}>
-                New user?{" "}
-                <Text style={{ color: "black", fontWeight: "bold" }}>
-                  Sign Up
-                </Text>
-              </Text>
-            </Button>
-          </Form>
+              Sign Up
+            </Text>
+          </TouchableOpacity>
         </View>
-    </>
+      </Animatable.View>
+    </View>
   );
 }
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: "white",
-//     justifyContent: "center",
-//   },
-//   header: {
-//     flex: 1,
-//     justifyContent: "flex-end",
-//     paddingHorizontal: 20,
-//     paddingBottom: 50,
-//     backgroundColor: "black"
-//   },
-//   footer: {
-//     flex: 3,
-//     backgroundColor: "#fff",
-//     borderTopLeftRadius: 30,
-//     borderTopRightRadius: 30,
-//     paddingHorizontal: 20,
-//     paddingVertical: 30,
-//   },
-
-//   title: {
-//     fontWeight: "bold",
-//     fontSize: 60,
-//     alignSelf: "center",
-//     marginTop: 15,
-//     color: "#FFB81D",
-//     fontFamily: "sans-serif-condensed",
-//   },
-
-//   image: {
-//     flex: 1,
-//     resizeMode: "cover",
-//     justifyContent: "center",
-//   },
-//   image1: {
-//     flex: 2,
-//     alignSelf: "center",
-//     margin: 60,
-//   },
-// });
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#009387'
+    backgroundColor: "#7c73e6",
   },
   header: {
-      flex: 1,
-      justifyContent: 'flex-end',
-      paddingHorizontal: 20,
-      paddingBottom: 50
+    flex: 2,
+    paddingHorizontal: 20,
+    paddingBottom: 50,
+    alignItems: 'center',
+    justifyContent: 'center'
   },
-
+  footer: {
+    flex: 3,
+    borderTopLeftRadius: 30,
+    borderTopRightRadius: 30,
+    paddingHorizontal: 20,
+    paddingVertical: 30,
+  },
   text_header: {
-      color: '#fff',
-      fontWeight: 'bold',
-      fontSize: 30
+    color: "#fafafa",
+    fontWeight: "bold",
+    fontSize: 30,
   },
   text_footer: {
-      color: '#05375a',
-      fontSize: 18
+    color: "#05375a",
+    fontSize: 18,
   },
   action: {
-      flexDirection: 'row',
-      marginTop: 10,
-      borderBottomWidth: 1,
-      borderBottomColor: '#f2f2f2',
-      paddingBottom: 5
+    flexDirection: "row",
+    marginTop: 10,
+    borderBottomWidth: 1,
+    borderBottomColor: "#f2f2f2",
+    paddingBottom: 5,
   },
   actionError: {
-      flexDirection: 'row',
-      marginTop: 10,
-      borderBottomWidth: 1,
-      borderBottomColor: '#FF0000',
-      paddingBottom: 5
+    flexDirection: "row",
+    marginTop: 10,
+    borderBottomWidth: 1,
+    borderBottomColor: "#FF0000",
+    paddingBottom: 5,
   },
   textInput: {
-      flex: 1,
-      marginTop: Platform.OS === 'ios' ? 0 : -12,
-      paddingLeft: 10,
-      color: '#05375a',
+    flex: 1,
+    marginTop: Platform.OS === "ios" ? 0 : -12,
+    paddingLeft: 10,
+    color: "#05375a",
   },
   errorMsg: {
-      color: '#FF0000',
-      fontSize: 14,
+    color: "#FF0000",
+    fontSize: 14,
   },
   button: {
-      alignItems: 'center',
-      marginTop: 50
+    alignItems: "center",
+    marginTop: 50,
   },
   signIn: {
-      width: '100%',
-      height: 50,
-      justifyContent: 'center',
-      alignItems: 'center',
-      borderRadius: 10
+    width: "100%",
+    height: 50,
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 10,
   },
   textSign: {
-      fontSize: 18,
-      fontWeight: 'bold'
-  }
+    fontSize: 18,
+    fontWeight: "bold",
+  },
+
+  logo: {
+    width: height_logo,
+    height: height_logo,
+
+  },
 });
